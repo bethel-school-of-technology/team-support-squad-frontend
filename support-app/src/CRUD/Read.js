7b58edf44304bb1dc3cdb221abbed08c9d0c0ffe
JSolano7 <jsolano5673@gmail.com>
2023-08-31 06:47:14 -0700

    Added the Maps to Read.js

--- support-app/src/CRUD/Read.js
+++ support-app/src/CRUD/Read.js
@@ -3,6 +3,8 @@ import { useParams, Link } from "react-router-dom";
 import { useItemContext } from "../context/ItemContext";
 import { useUserContext } from "../context/UserContext";
 import Delete from "./Delete"; // Import the Delete component
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"; // Import GoogleMap, LoadScript, and Marker
+import fetchCoordinates from "../components/ItemCoords";
 
 function Read() {
   const { user } = useUserContext();
@@ -10,11 +12,19 @@ function Read() {
   const { id } = useParams();
 
   const [item, setItem] = useState(null);
+  const [coordinates, setCoordinates] = useState(null);
 
   useEffect(() => {
     getItem(id)
       .then((itemData) => {
         setItem(itemData);
+        fetchCoordinates(itemData.address)
+          .then((coords) => {
+            setCoordinates(coords);
+          })
+          .catch((error) => {
+            console.error("Error fetching coordinates:", error);
+          });
       })
       .catch((error) => {
         console.error("Error fetching item data:", error);
@@ -35,6 +45,17 @@ function Read() {
       <p>Image: {item.image}</p>
       <p>Address: {item.address}</p>
       
+      {/* Render the Google Map */}
+        <LoadScript googleMapsApiKey="AIzaSyBU0ZdYhzfCYD0SJGYK72kDdw8xXFI2RK8">
+        <GoogleMap
+          mapContainerStyle={{ width: "100%", height: "300px" }}
+          center={coordinates || { lat: 0, lng: 0 }}
+          zoom={15}
+        >
+          {coordinates && <Marker position={coordinates} />}
+        </GoogleMap>
+      </LoadScript>
+
       {/* Conditionally render the Edit button */}
       {isOwner && 
       ( 
