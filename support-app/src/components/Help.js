5dc7e0b929e74e62037d9fae09c8742bdc0ae522
sonalkheware <sonalkhevare1997@gmail.com>
2023-09-01 15:38:28 +0530

    help.js css

--- support-app/src/components/Help.js
+++ support-app/src/components/Help.js
@@ -1,29 +1,56 @@
 import React from "react";
-import "../stylesheet/Help.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "../stylesheet/Help.css";
 import Map from "../components/Map";
 
 function Help() {
   return (
     <>
-    <div>
-      <h1>Help Center</h1>
-      <p>Welcome to the Help Center for our Offer App. We provide a platform for users to buy and sell their products easily.</p>
+    <div className="help-page">
+        <h1 className="help-title  blink">Help Center</h1>
+        <p className="help-description">Welcome to the Help Center for our Support App. We provide a platform for users to buy and sell their products easily.</p>
+      </div>
+      <div className="help-center-container">
+      <div className="section-card">
+        <div className="section">
+          <h3 className="section-title">About Our App</h3>
+          <p className="section-description">Our Support App is a user-friendly platform that enables you to showcase and sell your products online.</p>
+          <p>Our app provides you with the tools to create attractive offers and reach potential buyers.
+          </p>
+        </div>
+        </div>
+        <div className="section-card">
+        <div className="section">
+          <h3 className="section-title">Contact Us</h3>
+          <p className="section-description">If you can't find the information you're looking for, or if you need further assistance, our support team is here to help.</p>
+          <p className="contact-info">
+          Contact our support team at 
+          <span className="E-mail">
+        <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+        support@mail.com
+        </span> 
+        or call us at
+      <span className="phone-number">
+          <FontAwesomeIcon icon={faPhone} className="contact-icon" />
+          +1-800-123-4567</span></p>
+        </div>
+      </div>
+      </div>
+      <p className="availability">We are available during our working hours, Monday to Friday, 9 AM - 6 PM.</p>
 
-      <h2>About Our App</h2>
-      <p>Our Offer App is a user-friendly platform that enables you to showcase and sell your products online. Whether you're a business owner or an individual looking to sell items, our app provides you with the tools to create attractive offers and reach potential buyers.</p>
-      
-  <h2>Contact Us</h2>
-  <p>If you can't find the information you're looking for, or if you need further assistance, our support team is here to help.</p>
-  <p>Contact our support team at support@mail.com or call us at +1-800-123-4567.</p>
-  <p>We are available during our business hours, Monday to Friday, 9 AM - 6 PM.</p>
-
-  <h2>Seller Location</h2>
-  <div>
-    <Map />
-  </div>
-  </div>
- </>
+      <div className="map-card">
+        <div className="map-section">
+          <h2 className="section-title">Seller Location</h2>
+          <div className="map-container">
+            <Map />
+          </div>
+        </div>
+      </div>
+      </>
   );
 }
 
 export default Help;
+
+
