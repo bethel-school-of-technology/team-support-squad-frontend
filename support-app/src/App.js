6d195fa992f3e2f896148ea51ef842704ecd68ae
Rye-Rye <117476241+rcapacia@users.noreply.github.com>
2023-09-01 22:17:43 -0500

    9/1/23

--- support-app/src/App.js
+++ support-app/src/App.js
@@ -12,7 +12,8 @@ import Signup from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Read from "./CRUD/Read";
 import ItemList from "./components/ItemList";
-import UserProfile from './components/UserProfile';
+import UserProfile from "./components/UserProfile";
+import MockUp from "./components/MockUp";
 
 function App() {
   const handleCancel = () => {
@@ -33,9 +34,17 @@ function App() {
               <Route path="/sign-up" element={<Signup />} />
               <Route path="/sign-in" element={<SignIn />} />
               <Route path="/all" element={<ItemList />} />
+              <Route path="/test" element={<MockUp />} />
+
               <Route path="/items/:id" element={<Read />} />
-              <Route path="/create" element={<Create onCancel={handleCancel} />} />
-              <Route path="/update/:id" element={<Update onCancel={handleCancel} onEdit={handleEdit} />} />
+              <Route
+                path="/create"
+                element={<Create onCancel={handleCancel} />}
+              />
+              <Route
+                path="/update/:id"
+                element={<Update onCancel={handleCancel} onEdit={handleEdit} />}
+              />
               <Route path="/user-profile" element={<UserProfile />} />
               <Route path="/about" element={<About />} />
               <Route path="/help" element={<Help />} />
