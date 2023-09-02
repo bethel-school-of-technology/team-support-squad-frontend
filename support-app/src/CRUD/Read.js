ab0977959c4e4181eb99ef048d6a38c770bd175a
Rye-Rye <117476241+rcapacia@users.noreply.github.com>
2023-09-02 09:47:39 -0500

    single item page complete

--- support-app/src/CRUD/Read.js
+++ support-app/src/CRUD/Read.js
@@ -5,6 +5,7 @@ import { useUserContext } from "../context/UserContext";
 import Delete from "./Delete"; // Import the Delete component
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"; // Import GoogleMap, LoadScript, and Marker
 import fetchCoordinates from "../components/ItemCoords";
+import "../stylesheet/Read.css";
 
 function Read() {
   const { user } = useUserContext();
@@ -38,34 +39,58 @@ function Read() {
   const isOwner = user && item.user_id === user.id;
 
   return (
-    <div>
-      <h2>{item.name}</h2>
-      <p>{item.description}</p>
-      <p>Price: ${item.price}</p>
-      <p>Image: {item.image}</p>
-      <p>Address: {item.address}</p>
-      
-      {/* Render the Google Map */}
-        <LoadScript googleMapsApiKey="AIzaSyBU0ZdYhzfCYD0SJGYK72kDdw8xXFI2RK8">
-        <GoogleMap
-          mapContainerStyle={{ width: "100%", height: "300px" }}
-          center={coordinates || { lat: 0, lng: 0 }}
-          zoom={15}
-        >
-          {coordinates && <Marker position={coordinates} />}
-        </GoogleMap>
-      </LoadScript>
+    <div className="main-wrapper">
+      <div className="main-container">
+        <div className="item-div">
+          <div className="item-div-left">
+            <div className="img-container">
+              <img src={item.icon} alt="img" />
+            </div>
+          </div>
+          <div className="item-div-right">
+            <span className="item-name">{item.name}</span>
+            <span className="item-price">{item.price}</span>
+            <p class="item-description">
+              {item.description}Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Vitae animi ad minima veritatis dolore.
+              Architecto facere dignissimos voluptate fugit ratione molestias
+              quis quidem exercitationem voluptas.
+            </p>
+            <div class="btn-groups">
+              <button type="button" class="add-cart-btn">
+                <i class="fas fa-shopping-cart"></i>add to cart
+              </button>
+              <button type="button" class="buy-now-btn">
+                <i class="fas fa-wallet"></i>buy now
+              </button>
+            </div>
+            <br />
+            {/* <p>Address: {item.address}</p> */}
+            {/* Render the Google Map */}
+            <div className="map">
+              <LoadScript googleMapsApiKey="AIzaSyBU0ZdYhzfCYD0SJGYK72kDdw8xXFI2RK8">
+                <GoogleMap
+                  mapContainerStyle={{ width: "100%", height: "200px" }}
+                  center={coordinates || { lat: 0, lng: 0 }}
+                  zoom={15}
+                >
+                  {coordinates && <Marker position={coordinates} />}
+                </GoogleMap>
+              </LoadScript>
+            </div>
 
-      {/* Conditionally render the Edit button */}
-      {isOwner && 
-      ( 
-        <Link to={`/update/${id}`}>
-          <button>Edit</button>
-        </Link>
-      )}
+            {/* Conditionally render the Edit button */}
+            {isOwner && (
+              <Link to={`/update/${id}`}>
+                <button>Edit</button>
+              </Link>
+            )}
 
-      {/* Conditionally render the Delete component */}
-      {isOwner && <Delete id={id} />}
+            {/* Conditionally render the Delete component */}
+            {isOwner && <Delete id={id} />}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
